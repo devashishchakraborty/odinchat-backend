c08b8239fe3b8c32b9ef39d992f2848f9ea0dc8b
Devashish Chakraborty <devashishchakra@gmail.com>
2025-04-09 00:18:41 +0530

    Sending the latest message along with the user to show in users panel frontend

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,15 +4,50 @@ const prisma = new PrismaClient();
 
 const getUsers = async (req, res) => {
   const users = await prisma.user.findMany({
+    where: { NOT: { id: req.user.id } },
     select: {
       id: true,
-      name: true, 
-      email: true
-    }
+      name: true,
+      email: true,
+      messages_sent: {
+        where: {
+          receiver_id: req.user.id,
+        },
+        orderBy: { created_at: "desc" },
+        take: 1,
+      },
+      messages_received: {
+        where: {
+          author_id: req.user.id,
+        },
+        orderBy: { created_at: "desc" },
+        take: 1,
+      },
+    },
   });
 
   if (!users) return res.sendStatus(500);
-  res.send(users);
+
+  const usersWithLatestMessage = users.map((user) => {
+    const sent = user.messages_sent[0];
+    const received = user.messages_received[0];
+
+    let latestMessage = null;
+    if (sent && received) {
+      latestMessage = sent.created_at > received.created_at ? sent : received;
+    } else {
+      latestMessage = sent || received || null;
+    }
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      latestMessage,
+    };
+  });
+
+  res.send(usersWithLatestMessage);
 };
 
 const deleteUser = async (req, res) => {
