aee3596a11ee8ec208a6c5783193b11e7fd6f4fb
Devashish Chakraborty <devashishchakra@gmail.com>
2025-04-13 04:30:02 +0530

    Separated the socket.io configuration into a different file.

--- index.js
+++ index.js
@@ -5,68 +5,21 @@ import cors from "cors";
 import authenticateJWT from "./middlewares/authenticateToken.js";
 import { Server } from "socket.io";
 import { createServer } from "http";
-import jwt from "jsonwebtoken";
-import { PrismaClient } from "@prisma/client";
+import configureSocket from "./config/socket.js";
 import "dotenv/config";
+
 
 const app = express();
 const server = createServer(app);
 const port = process.env.PORT || 3000;
-const prisma = new PrismaClient();
 const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_BASE_URL,
     methods: ["GET", "POST"],
-    credentials: true,
   },
 });
 
-io.use((socket, next) => {
-  const token = socket.handshake.auth.token;
-
-  if (!token) {
-    return next(new Error("Authentication error: token missing"));
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    socket.user = decoded;
-    next();
-  } catch (err) {
-    next(new Error("Authentication error: token invalid"));
-  }
-});
-
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.user.id);
-
-  // Join a room (e.g., private chat between two users)
-  socket.on("join room", ({ senderId, receiverId }) => {
-    const roomId = [senderId, receiverId].sort().join("-");
-    socket.join(roomId);
-  });
-
-  socket.on("send message", async (data) => {
-    console.log(`User ${socket.user.id} sent:`, data.text);
-    const { text, authorId, receiverId } = data;
-    const roomId = [authorId, receiverId].sort().join("-");
-
-    const message = await prisma.message.create({
-      data: {
-        text,
-        author_id: authorId,
-        receiver_id: receiverId,
-      },
-    });
-
-    io.to(roomId).emit("receive message", message);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User Disconnected: " + socket.user.id);
-  });
-});
-
+configureSocket(io);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(import.meta.dirname, "public")));
